eba6c8626670ca01e37bf477f0d5b173c070acb5
Kenneth <94906413+kenedy69@users.noreply.github.com>
2022-12-02 10:31:19 +0100

    new file:   vite-project/.gitignore 	new file:   vite-project/index.html 	new file:   vite-project/package-lock.json       new file:   vite-project/package.json       new file:   vite-project/src/css/style.css 	new file:   vite-project/src/main.ts 	new file:   vite-project/src/model/ListItem.ts 	new file:   vite-project/src/vite-env.d.ts 	new file:   vite-project/tsconfig.json

---
+++ vite-project/src/model/ListItem.ts
@@ -0,0 +1,22 @@
+export interface item {
+      id: string,
+      item: string,
+      checked: boolean
+}
+
+export default class ListItem implements Item {
+      constructor(
+            private _id: string = '',
+            private _item: string = '',
+            private _checked: boolean = false,
+            
+      ){}
+
+      get id(): string {
+            return this._id
+      }
+
+      set id(id: string) {
+            this._id = id
+      }
+}
