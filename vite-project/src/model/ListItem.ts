f96a657329c1d3e17d963130b27bab6337bee807
Kenneth <94906413+kenedy69@users.noreply.github.com>
2022-12-02 10:42:15 +0100

    modified:   vite-project/src/model/ListItem.ts vite-project/src/model/FullList.ts

--- vite-project/src/model/ListItem.ts
+++ vite-project/src/model/ListItem.ts
@@ -19,4 +19,19 @@ export default class ListItem implements Item {
       set id(id: string) {
             this._id = id
       }
+      get item(): string {
+            return this._item
+        }
+    
+      set item(item: string) {
+            this._item = item
+        }
+    
+      get checked(): boolean {
+            return this._checked
+        }
+    
+      set checked(checked: boolean) {
+            this._checked = checked
+        }
 }
